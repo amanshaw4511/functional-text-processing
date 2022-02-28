4362bd838fb78fe1bad6648d523062d417a17c20
amanshaw4511 <amanshaw4511@protonmail.com>
2022-02-28 23:00:41 +0530

    added generate command

---
+++ generate.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+start = sys.argv[1]
+step = sys.argv[2]
+stop = sys.argv[3]
+
+_init = eval(start)
+_ = _init
+while (eval(stop)) :
+    print(_)
+    _ = eval(step)
