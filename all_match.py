a32edf6517527c2588fad92eb0afc14c78266fde
amanshaw4511 <amanshaw4511@protonmail.com>
2022-03-01 00:44:59 +0530

    added all_match, any_match, none_match command

---
+++ all_match.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+fn = sys.argv[1]
+
+lines = map(str.strip, sys.stdin)
+
+for _ in lines :
+    if eval(fn) :
+        continue
+    else :
+        exit()
+print(1)
